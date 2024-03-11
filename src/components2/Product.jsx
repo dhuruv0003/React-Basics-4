1b81ec07d6adb50e3fd7c20c3fd5aba63755dc00
dhuruv0003 <116896454+dhuruv0003@users.noreply.github.com>
2024-03-12 02:26:06 +0530

    "new commit"

---
+++ src/components2/Product.jsx
@@ -0,0 +1,14 @@
+import ProductItem from './ProductItem'
+import Card from './Card'
+
+export default function Product(props){
+    return(
+        <Card className="products">
+            <ProductItem title={props.items[0].title}
+            price={props.itmes[0].price} date={props.items[0].date}></ProductItem>
+            <ProductItem title={props.items[1].title} price={props.items[1].price} date={props.items[1].date}></ProductItem>
+            <ProductItem title={props.items[2].title} price={props.items[2].price} date={props.items[2].date}></ProductItem>
+            <ProductItem title={props.items[3].title} price={props.items[3].price} date={props.items[3].date}></ProductItem>
+        </Card>
+    )
+}
