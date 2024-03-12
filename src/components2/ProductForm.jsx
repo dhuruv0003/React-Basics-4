10a34778d10885d3aa42d4ab7f831c726284f912
dhuruv0003 <116896454+dhuruv0003@users.noreply.github.com>
2024-03-13 01:19:48 +0530

    "new commit"

--- src/components2/ProductForm.jsx
+++ src/components2/ProductForm.jsx
@@ -1,16 +1,22 @@
+import './ProductForm.css'
+
 export default function ProductForm() {
     return (
         <form action="">
-            <div className="new-product-title">
-                <label htmlFor="title">Title</label>
-                <input type="text" name="title" />
-            </div>
-            <div className="new-product-date">
-                <label htmlFor="date">Date</label>
-                <input type="date" name="date" min='2023-01-01' max='2023-12-12'/>
-            </div>
-            <div className="new-product-btn">
-                <button type="submit">Add Product</button>
+            
+            <div className="new-product-form">
+            <div className="form-container"><div className="new-product-title">
+                    <label htmlFor="title">Title</label>
+                    <input type="text" name="title" />
+                </div>
+                <div className="new-product-date">
+                    <label htmlFor="date">Date</label>
+                    <input type="date" name="date" min='2023-01-01' max='2023-12-12' />
+                </div></div>
+
+                <div className="new-product-btn">
+                    <button type="submit">Add Product</button>
+                </div>
             </div>
         </form>
 
