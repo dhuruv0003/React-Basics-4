8330cdbc58c1417b395824485f4646d9ed29e2b8
dhuruv0003 <116896454+dhuruv0003@users.noreply.github.com>
2024-03-13 02:12:30 +0530

    "commit"

--- src/components2/ProductForm.jsx
+++ src/components2/ProductForm.jsx
@@ -7,21 +7,26 @@ export default function ProductForm() {
         date:''
     } 
     let [input,setinput]=useState(ob)
-    function changehandler(event){
-        setinput({...input,title:event.target.value})
-        
+    function titlehandler(event){
+        setinput({...input,title:event.target.value})   
+    }
+    function datehandler(event){
+        setinput({...input,date:event.target.date})
+    }
+    function submithandle(event){
+        event.preventDefault();
     }
     return (
-        <form action="">
+        <form action="" onSubmit={submithandle}>
             
             <div className="new-product-form">
             <div className="form-container"><div className="new-product-title">
                     <label htmlFor="title">Title</label>
-                    <input type="text" name="title" onChange={changehandler} />{input.title}
+                    <input type="text" name="title" onChange={titlehandler} />{input.title}
                 </div>
                 <div className="new-product-date">
                     <label htmlFor="date">Date</label>
-                    <input type="date" name="date" min='2023-01-01' max='2023-12-12' />
+                    <input type="date" name="date" min='2023-01-01' max='2023-12-12' onChange={datehandler}/>
                 </div></div>
 
                 <div className="new-product-btn">
