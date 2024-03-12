dc68b8087df778f3ced939dd7493a035b3ab77d9
dhuruv0003 <116896454+dhuruv0003@users.noreply.github.com>
2024-03-13 02:45:59 +0530

    "new commit"

--- src/components2/ProductForm.jsx
+++ src/components2/ProductForm.jsx
@@ -2,32 +2,56 @@ import { useState } from 'react'
 import './ProductForm.css'
 
 export default function ProductForm() {
-    const ob={
-        title:'',
-        date:''
-    } 
-    let [input,setinput]=useState(ob)
+    // let ob = {
+    //     newtitle: '',
+    //     newdate: ''
+    // }
+    // let [input, setinput] = useState(ob)
+    // function titlehandler(event) {
+    //     setinput((newinp) => {
+    //         return ({ ...newinp, newtitle: event.target.value })
+    //     })  
+    // }
+    // function datehandler(event) {
+    //     setinput((newdate) => {
+    //         return ({ ...newdate, newdate: event.target.value })
+    //     })  
+    // }
+    const[newtitle,settitle]=useState("");
+    const[newdate,setdate]=useState("");
+
     function titlehandler(event){
-        setinput({...input,title:event.target.value})   
+        settitle(event.target.value)
     }
     function datehandler(event){
-        setinput({...input,date:event.target.date})
+        setdate(event.target.value)
     }
-    function submithandle(event){
+
+
+    function submithandle(event) {
         event.preventDefault();
+        const obj = {
+            title: newtitle,
+            date: newdate
+        }
+        console.log(obj);
+    //After adding we want to have empty input again
+    //so to do this setrmpty to both
+        setdate("");
+        settitle("")
     }
     return (
         <form action="" onSubmit={submithandle}>
-            
+
             <div className="new-product-form">
-            <div className="form-container"><div className="new-product-title">
+                <div className="form-container"><div className="new-product-title">
                     <label htmlFor="title">Title</label>
-                    <input type="text" name="title" onChange={titlehandler} />{input.title}
+                    <input type="text" value={newtitle}name="title" onChange={titlehandler} />
                 </div>
-                <div className="new-product-date">
-                    <label htmlFor="date">Date</label>
-                    <input type="date" name="date" min='2023-01-01' max='2023-12-12' onChange={datehandler}/>
-                </div></div>
+                    <div className="new-product-date">
+                        <label htmlFor="date">Date</label>
+                        <input type="date" name="date" min='2023-01-01' max='2023-12-12' value={newdate} onChange={datehandler} />
+                    </div></div>
 
                 <div className="new-product-btn">
                     <button type="submit">Add Product</button>
