359ad0c5ed69aa13696da87184ce7a7490a6dff2
dhuruv0003 <116896454+dhuruv0003@users.noreply.github.com>
2024-03-12 18:36:52 +0530

    "new commit"

--- src/components2/ProductItem.jsx
+++ src/components2/ProductItem.jsx
@@ -1,17 +1,23 @@
 
+import { useState } from 'react';
 import ProductDate from './ProductDate'
 
 import './ProductItem.css'
 
 export default function ProductItem(props) {
+
+    
+    let [title,setTitle]=useState(props.title)
+
     function Handleclick(){
+        setTitle("Popcorn")
         console.log("Button was clicked");
     }
     return (
         <div className='product-item'>
             <ProductDate date={props.date}></ProductDate>
             <div className='product-item-desc'>
-                <h2>{props.title}</h2>
+                <h2>{title}</h2>
             </div>
             <button onClick={Handleclick}>Add To Cart</button>
         </div>
